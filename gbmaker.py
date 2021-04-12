03d31ccfeba40b6e573f5f9e79d97466c4cb9009
Adam Kerrigan <ak1014@york.ac.uk>
2021-04-12 11:38:24 +0100

    passed all python through autopep8

--- gbmaker.py
+++ gbmaker.py
@@ -1,95 +1,98 @@
-#works with pymatgen Version: 2019.1.24
+# works with pymatgen Version: 2019.1.24
 import numpy
 import pymatgen
 from pymatgen.core.surface import Structure, Slab, SlabGenerator
 from pymatgen.transformations.standard_transformations import RotationTransformation
 
-def genslab(slaboriginal,vacuum=0):
-  #rotate cell so that surface normal along z
-  axis_rot=numpy.cross(slaboriginal.normal,[0,0,1])
-  angle=numpy.arccos(numpy.dot(slaboriginal.normal,[0,0,1]))
-  SlabRot=RotationTransformation(axis_rot,angle,angle_in_radians=True)
-  slab2=SlabRot.apply_transformation(slaboriginal)
-  slaboriginal=slab2
-  #extract coords
-  sites=list(slaboriginal.sites)
-  sortedzsites = sorted(sites, key=lambda site: site.coords[2])
-  mingrain=sortedzsites[0].coords[2]
-  maxgrain=sortedzsites[-1].coords[2]
-  
-  lattice=slaboriginal.lattice
-  newlatticematrix=lattice.matrix.copy()
-  newlatticematrix[2][2]=(maxgrain-mingrain)+vacuum
-  newlatticematrix[2][1]=0
-  
-  all_species =[]
-  all_species.extend(site.specie for site in sortedzsites)
 
-  all_coords =[]
-  for i in range(0,len(sortedzsites)):
-    newcoord=sortedzsites[i].coords
-    newcoord[0]=newcoord[0]
-    newcoord[1]=newcoord[1]
-    newcoord[2]=newcoord[2]-mingrain
-    all_coords.append(newcoord)
+def genslab(slaboriginal, vacuum=0):
+    # rotate cell so that surface normal along z
+    axis_rot = numpy.cross(slaboriginal.normal, [0, 0, 1])
+    angle = numpy.arccos(numpy.dot(slaboriginal.normal, [0, 0, 1]))
+    SlabRot = RotationTransformation(axis_rot, angle, angle_in_radians=True)
+    slab2 = SlabRot.apply_transformation(slaboriginal)
+    slaboriginal = slab2
+    # extract coords
+    sites = list(slaboriginal.sites)
+    sortedzsites = sorted(sites, key=lambda site: site.coords[2])
+    mingrain = sortedzsites[0].coords[2]
+    maxgrain = sortedzsites[-1].coords[2]
 
-  slab=Structure(newlatticematrix,all_species,all_coords,coords_are_cartesian=True)
+    lattice = slaboriginal.lattice
+    newlatticematrix = lattice.matrix.copy()
+    newlatticematrix[2][2] = (maxgrain-mingrain)+vacuum
+    newlatticematrix[2][1] = 0
 
-  return slab
+    all_species = []
+    all_species.extend(site.specie for site in sortedzsites)
+
+    all_coords = []
+    for i in range(0, len(sortedzsites)):
+        newcoord = sortedzsites[i].coords
+        newcoord[0] = newcoord[0]
+        newcoord[1] = newcoord[1]
+        newcoord[2] = newcoord[2]-mingrain
+        all_coords.append(newcoord)
+
+    slab = Structure(newlatticematrix, all_species,
+                     all_coords, coords_are_cartesian=True)
+
+    return slab
 
 
-def gengb(slaboriginal_grain1,slaboriginal_grain2,GB_x_shift=0,GB_y_shift=0,GB_z_shift1=0,GB_z_shift2=0,MergeOn=False,merge_tol=1.0,MirrorX=False,MirrorY=False,MirrorZ=True):
-  #rotate cell so that surface normal along z
-  axis_rot=numpy.cross(slaboriginal_grain1.normal,[0,0,1])
-  angle=numpy.arccos(numpy.dot(slaboriginal_grain1.normal,[0,0,1]))
-  SlabRot=RotationTransformation(axis_rot,angle,angle_in_radians=True)
-  slab2=SlabRot.apply_transformation(slaboriginal_grain1)
-  slaboriginal_grain1=slab2
-  slab2=SlabRot.apply_transformation(slaboriginal_grain2)
-  slaboriginal_grain2=slab2
-  #extract coords
-  sites=list(slaboriginal_grain1.sites)
-  sortedzsitesg1 = sorted(sites, key=lambda site: site.coords[2])
-  mingrain1=sortedzsitesg1[0].coords[2]
-  maxgrain1=sortedzsitesg1[-1].coords[2]
-  sites=list(slaboriginal_grain2.sites)
-  sortedzsitesg2 = sorted(sites, key=lambda site: site.coords[2])
-  mingrain2=sortedzsitesg2[0].coords[2]
-  maxgrain2=sortedzsitesg2[-1].coords[2]
-  
-  lattice=slaboriginal_grain1.lattice
-  newlatticematrix=lattice.matrix.copy()
-  newlatticematrix[2][2]=(maxgrain1-mingrain1)+(maxgrain2-mingrain2)+GB_z_shift1+GB_z_shift2
-  newlatticematrix[2][1]=0
-  #print(newlatticematrix)
-  
-  all_species =[]
-  all_species.extend(site.specie for site in sortedzsitesg1)
-  all_species.extend(site.specie for site in sortedzsitesg2)
+def gengb(slaboriginal_grain1, slaboriginal_grain2, GB_x_shift=0, GB_y_shift=0, GB_z_shift1=0, GB_z_shift2=0, MergeOn=False, merge_tol=1.0, MirrorX=False, MirrorY=False, MirrorZ=True):
+    # rotate cell so that surface normal along z
+    axis_rot = numpy.cross(slaboriginal_grain1.normal, [0, 0, 1])
+    angle = numpy.arccos(numpy.dot(slaboriginal_grain1.normal, [0, 0, 1]))
+    SlabRot = RotationTransformation(axis_rot, angle, angle_in_radians=True)
+    slab2 = SlabRot.apply_transformation(slaboriginal_grain1)
+    slaboriginal_grain1 = slab2
+    slab2 = SlabRot.apply_transformation(slaboriginal_grain2)
+    slaboriginal_grain2 = slab2
+    # extract coords
+    sites = list(slaboriginal_grain1.sites)
+    sortedzsitesg1 = sorted(sites, key=lambda site: site.coords[2])
+    mingrain1 = sortedzsitesg1[0].coords[2]
+    maxgrain1 = sortedzsitesg1[-1].coords[2]
+    sites = list(slaboriginal_grain2.sites)
+    sortedzsitesg2 = sorted(sites, key=lambda site: site.coords[2])
+    mingrain2 = sortedzsitesg2[0].coords[2]
+    maxgrain2 = sortedzsitesg2[-1].coords[2]
 
-  all_coords =[]
-  for i in range(0,len(sortedzsitesg1)):
-    newcoord=sortedzsitesg1[i].coords
-    newcoord[0]=newcoord[0]+GB_x_shift
-    newcoord[1]=newcoord[1]+GB_y_shift
-    newcoord[2]=newcoord[2]-mingrain1+(GB_z_shift1/2.0) 
-    all_coords.append(newcoord)
-  for i in range(0,len(sortedzsitesg2)):
-    newcoord=sortedzsitesg2[i].coords
-    if MirrorX:
-      newcoord[0]=newcoord[0]*-1
-    if MirrorY:
-      newcoord[1]=newcoord[1]*-1
-    if MirrorZ:
-      newcoord[2]=(newcoord[2]-mingrain2+GB_z_shift1/2.0)*-1
-    else:
-      newcoord[2]=(newcoord[2]-mingrain2+(maxgrain1-mingrain1)+GB_z_shift2+GB_z_shift1/2.0)
-    all_coords.append(newcoord)
+    lattice = slaboriginal_grain1.lattice
+    newlatticematrix = lattice.matrix.copy()
+    newlatticematrix[2][2] = (maxgrain1-mingrain1) + \
+        (maxgrain2-mingrain2)+GB_z_shift1+GB_z_shift2
+    newlatticematrix[2][1] = 0
+    # print(newlatticematrix)
 
-  GBSlab=Structure(newlatticematrix,all_species,all_coords,coords_are_cartesian=True)
+    all_species = []
+    all_species.extend(site.specie for site in sortedzsitesg1)
+    all_species.extend(site.specie for site in sortedzsitesg2)
 
-  if MergeOn:
-    GBSlab.merge_sites(merge_tol,mode="delete")
-  return GBSlab
-  
-  
+    all_coords = []
+    for i in range(0, len(sortedzsitesg1)):
+        newcoord = sortedzsitesg1[i].coords
+        newcoord[0] = newcoord[0]+GB_x_shift
+        newcoord[1] = newcoord[1]+GB_y_shift
+        newcoord[2] = newcoord[2]-mingrain1+(GB_z_shift1/2.0)
+        all_coords.append(newcoord)
+    for i in range(0, len(sortedzsitesg2)):
+        newcoord = sortedzsitesg2[i].coords
+        if MirrorX:
+            newcoord[0] = newcoord[0]*-1
+        if MirrorY:
+            newcoord[1] = newcoord[1]*-1
+        if MirrorZ:
+            newcoord[2] = (newcoord[2]-mingrain2+GB_z_shift1/2.0)*-1
+        else:
+            newcoord[2] = (newcoord[2]-mingrain2+(maxgrain1 -
+                                                  mingrain1)+GB_z_shift2+GB_z_shift1/2.0)
+        all_coords.append(newcoord)
+
+    GBSlab = Structure(newlatticematrix, all_species,
+                       all_coords, coords_are_cartesian=True)
+
+    if MergeOn:
+        GBSlab.merge_sites(merge_tol, mode="delete")
+    return GBSlab
--- tests/CuPbSbS3/011/ManualGB.py
+++ tests/CuPbSbS3/011/ManualGB.py
@@ -1,4 +1,4 @@
-#works with pymatgen Version: 2019.1.24
+# works with pymatgen Version: 2019.1.24
 import numpy
 import pymatgen
 import subprocess
@@ -14,60 +14,63 @@ from gbmaker import *
 # JOB SPECIFICATION     #
 #########################
 
-#Note: intial bulk structure to be provided as POSCAR-bulk in working directory
+# Note: intial bulk structure to be provided as POSCAR-bulk in working directory
 
-#GB definition
-gb_plane=[0,1,1]  #GB plane
-slab_min_t=3.5     #slab thickness in hkl units
-GB_a_shift=0.000  #relative grain shift in a parallel to GB
-GB_b_shift=0.000  #relative grain shift in b parallel to GB
-GB_z_shift1=2.0   #separation in z between teminating plabes for GB 1
-GB_z_shift2=2.0   #separation in z between teminating plabes for GB 2
+# GB definition
+gb_plane = [0, 1, 1]  # GB plane
+slab_min_t = 3.5  # slab thickness in hkl units
+GB_a_shift = 0.000  # relative grain shift in a parallel to GB
+GB_b_shift = 0.000  # relative grain shift in b parallel to GB
+GB_z_shift1 = 2.0  # separation in z between teminating plabes for GB 1
+GB_z_shift2 = 2.0  # separation in z between teminating plabes for GB 2
 
-Symmetric=False  #whether slabs should be symmetric
-Grain1_slabid=0  #grain1 id
-Grain2_slabid=0  #grain2 id
+Symmetric = False  # whether slabs should be symmetric
+Grain1_slabid = 0  # grain1 id
+Grain2_slabid = 0  # grain2 id
 
-#Grain2 transformation (e.g. for sym tilt GB MirrorX=False, MirrorY=False, MirrorZ=True)
-MirrorX=False
-MirrorY=True
-MirrorZ=False
+# Grain2 transformation (e.g. for sym tilt GB MirrorX=False, MirrorY=False, MirrorZ=True)
+MirrorX = False
+MirrorY = True
+MirrorZ = False
 
-MergeOn=False    #whether or not to merge atoms if closer than merge_tol
-merge_tol=1.5    #distance for which atoms are merged (if using)
+MergeOn = False  # whether or not to merge atoms if closer than merge_tol
+merge_tol = 1.5  # distance for which atoms are merged (if using)
 
-vacuum=5.0       #vacuum gap to use for output of slab structures
+vacuum = 5.0  # vacuum gap to use for output of slab structures
 
 #######################################
 # MAIN CODE                           #
 #######################################
 
-#Read and analyse bulk structure provided
+# Read and analyse bulk structure provided
 prim = pymatgen.Structure.from_str(open("POSCAR-bulk").read(), fmt="poscar")
 sga = SpacegroupAnalyzer(prim)
-bulk=sga.get_conventional_standard_structure() #conventional needed for surface cells
+# conventional needed for surface cells
+bulk = sga.get_conventional_standard_structure()
 print('STRUCTURE PROVIDED:')
 print(bulk)
 print()
 
-#Generate surf slabs and output for visualisation
-SlabGen=SlabGenerator(bulk,gb_plane,slab_min_t,slab_min_t,lll_reduce=False, center_slab=False, in_unit_planes=True, primitive=True, max_normal_search=None,reorient_lattice=True)
-slabs=SlabGen.get_slabs(symmetrize=Symmetric)
+# Generate surf slabs and output for visualisation
+SlabGen = SlabGenerator(bulk, gb_plane, slab_min_t, slab_min_t, lll_reduce=False, center_slab=False,
+                        in_unit_planes=True, primitive=True, max_normal_search=None, reorient_lattice=True)
+slabs = SlabGen.get_slabs(symmetrize=Symmetric)
 print('SURFACE SLABS:')
-print('Num slabs: ',len(slabs))
-for i in range(0,len(slabs)):
-  print('Slab ', i, ', symmetric: ',slabs[i].is_symmetric())
-  slab=genslab(slabs[i],vacuum=vacuum)
-  slab.get_sorted_structure().to(filename='POSCAR-slab-{}'.format(i), fmt="poscar")
+print('Num slabs: ', len(slabs))
+for i in range(0, len(slabs)):
+    print('Slab ', i, ', symmetric: ', slabs[i].is_symmetric())
+    slab = genslab(slabs[i], vacuum=vacuum)
+    slab.get_sorted_structure().to(filename='POSCAR-slab-{}'.format(i), fmt="poscar")
 print()
 
 
-#Construct and output POSCAR for GB
+# Construct and output POSCAR for GB
 print('GRAIN BOUNDARY CONSTRUCTION:')
-latticematrix=slab.lattice.matrix  
-GB_x_shift=GB_a_shift*latticematrix[0][0]+GB_b_shift*latticematrix[1][0]
-GB_y_shift=GB_a_shift*latticematrix[0][1]+GB_b_shift*latticematrix[1][1]
-GBSlab=gengb(slabs[Grain1_slabid],slabs[Grain2_slabid],GB_x_shift,GB_y_shift,GB_z_shift1,GB_z_shift2,MergeOn,merge_tol,MirrorX,MirrorY,MirrorZ)
+latticematrix = slab.lattice.matrix
+GB_x_shift = GB_a_shift*latticematrix[0][0]+GB_b_shift*latticematrix[1][0]
+GB_y_shift = GB_a_shift*latticematrix[0][1]+GB_b_shift*latticematrix[1][1]
+GBSlab = gengb(slabs[Grain1_slabid], slabs[Grain2_slabid], GB_x_shift, GB_y_shift,
+               GB_z_shift1, GB_z_shift2, MergeOn, merge_tol, MirrorX, MirrorY, MirrorZ)
 print('Composition: ', GBSlab.composition.formula)
 print('Reduced composition: ', GBSlab.composition.reduced_formula)
-GBSlab.get_sorted_structure().to(filename='POSCAR-GB', fmt="poscar")  
+GBSlab.get_sorted_structure().to(filename='POSCAR-GB', fmt="poscar")
