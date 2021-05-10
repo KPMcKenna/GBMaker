6b0c48dd8e9f1add114d1a47c2502bc2c7fc9a13
Adam Kerrigan <ak1014@york.ac.uk>
2021-05-10 13:15:42 +0100

    document domain wall example

--- examples/CuPbSbS3/011_domain_wall.py
+++ examples/CuPbSbS3/011_domain_wall.py
@@ -2,23 +2,28 @@ from gbmaker import GrainGenerator, GrainBoundary
 from pymatgen.core import Structure
 from pymatgen.vis.structure_vtk import MultiStructuresVis
 
+# load the bulk cell
 bulk = Structure.from_file("./POSCAR-bulk")
 
+# generate the grains
 gg = GrainGenerator(bulk, [0, 1, 1])
-
 grains = gg.get_grains()
-grains[0].orthogonalise_c()
 
+# view the grains
 view = MultiStructuresVis()
 view.set_structures(grains)
 view.show()
 
-translation_vec = grains[0].scaled_c_vector / grains[0].thickness
-translation_vec *= 2.0
+# orthogonalise the c-vector so that each boundary is symmetric
+grains[0].orthogonalise_c()
 
+# seperate the grains by 2 Angstrom
+translation_vec = [0.0, 0.0, 2.0]
+
+# create a domain wall
 gb = GrainBoundary(grain_1=grains[0], mirror_y=True, translation_vec=translation_vec)
 gb.grain_1.hkl_thickness = 3.5
 gb.grain_2.hkl_thickness = 3.5
-gb.as_structure().get_sorted_structure().to("poscar", "011_grainboundary.vasp")
+gb.as_structure().get_sorted_structure().to("poscar", "011_domain_wall.vasp")
 gb.grain_1.to("poscar", "./grain_0.vasp")
 gb.grain_2.to("poscar", "./grain_1.vasp")
