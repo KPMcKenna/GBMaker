1ee08d90d64c93c779a45e96c77287420db87252
Keith McKenna <keith.mckenna@york.ac.uk>
2021-06-02 15:05:09 +0100

    added FAPbI3 example but repair not working

---
+++ examples/FAPbI3/210_grain_boundary.py
@@ -0,0 +1,16 @@
+from pymatgen.core import Structure
+from gbmaker import GrainGenerator, GrainBoundary
+
+bondlengths ={("Pb","I"): 3.2,("N","H"): 1.05,("C","H"): 1.10, ("N","C"): 1.35}
+
+bulk = Structure.from_file(filename="./POSCAR-bulk")
+gg = GrainGenerator(bulk, [2,1,0], symmetrize=False,repair=True,bonds=bondlengths)
+grains = gg.get_grains()
+
+#visualise to determine appropriate termination
+for i, g in enumerate(grains):
+    g.bulk_repeats=1
+    g.get_sorted_structure().to("poscar", f"{i}-210_grain.vasp")
+
+
+
